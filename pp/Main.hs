42fb2ec56d97d009e7cdfe4fec4ba55bd6c9de5f
David Johnson <djohnson.m@gmail.com>
2020-07-07 17:22:40 -0400

    Add postprocessing.

--- pp/Main.hs
+++ pp/Main.hs
@@ -1,13 +1,17 @@
+{-# LANGUAGE ViewPatterns #-}
 module Main where
 
+import Debug.Trace
+import GHC.Plugin.LensThRewrite
 import System.Environment
 import System.Exit
-import GHC.Plugin.LensThRewrite
 
 import Language.Haskell.GHC.ExactPrint.Parsers
+import Language.Haskell.GHC.ExactPrint
 
+import GHC                                     hiding (parseModule)
 import CoreSyn
-import GhcPlugins
+import GhcPlugins                              hiding ((<>))
 import HsDecls
 import HsDumpAst
 import HsExtension
@@ -26,10 +30,39 @@ main = do
 
 go :: String -> String -> IO ()
 go input output = do
+  putStrLn "running....."
   result <- parseModule input
   case result of
-    Right (_, L _ s) -> do
+    Right (k, L x s) -> do
       let n = rewriteModule s
-      writeFile output $ showSDocUnsafe (ppr n)
+          txt = postProcess $ showSDocUnsafe (ppr n)
+      writeFile output txt
     Left s -> print s >> exitFailure
 
+postProcess
+  :: String
+  -> String
+postProcess
+  = unlines
+  . fmap rewriteLine
+  . lines
+
+rewriteLine
+  :: String
+  -> String
+rewriteLine original@(words -> xs) =
+  if "::" `elem` xs && "Lens'" `elem` xs
+    then
+      case xs of
+        name : "::" : "Lens'" : innerType : rest ->
+          unwords $
+          [ name
+          , "::"
+          , "Lens'"
+          , innerType
+          ] ++ ("(" : rest ++ [")"])
+        _ -> error $ "rewriteLine: Bad match: " <> original
+    else
+      if "Lens" `elem` xs
+        then traceShow ("failed: " ++ original) original
+        else original
